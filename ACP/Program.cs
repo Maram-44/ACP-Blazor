15b9108b5682f448de3a9b18546c2830c5f179e2
Taiba-Sa <s22170211010@hu.edu.ye>
2026-02-06 20:34:35 +0300

    animal details

--- ACP/Program.cs
+++ ACP/Program.cs
@@ -1,11 +1,15 @@
 using ACP;
 using ACP.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddAuthorizationCore();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<AuthenticationStateProvider, SmartAuthStateProvider>();
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
