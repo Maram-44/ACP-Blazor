acdf1d50420e62776fa6b73fc16e6996bbef4101
thfatima <fatimh18844@gmail.com>
2026-02-06 19:07:50 +0300

    Browse Medical Center (medical + grooming)

---
+++ ACP/Components/BrowseMedical/MedicalFilter.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ACP.Components.MedicalCenters;
+
+public partial class MedicalFilter
+{
+    private string SelectedCity { get; set; } = "all";
+    private string SelectedService { get; set; } = "all";
+
+    // نمرر قائمة المدن من الصفحة الرئيسية ليكون الفلتر مرن
+    [Parameter] public List<string>? Cities { get; set; }
+
+    [Parameter] public EventCallback<(string city, string service)> OnFilterChanged { get; set; }
+
+    private async Task SetService(string service)
+    {
+        SelectedService = service;
+        await AutoApply();
+    }
+
+    private async Task AutoApply()
+    {
+        if (OnFilterChanged.HasDelegate)
+        {
+            await OnFilterChanged.InvokeAsync((SelectedCity, SelectedService));
+        }
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await AutoApply();
+        }
+    }
+}
