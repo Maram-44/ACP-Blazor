d12e3da7df700b42d3f6874cbfa286ebb0630b2b
Maram-44 <maramaljoohy@gmail.com>
2026-02-25 12:01:42 +0300

    Subscription

---
+++ ACP/Models/Customers/CustomerSubscription.cs
@@ -0,0 +1,12 @@
+﻿namespace ACP.Models.Customers
+{
+    public class CustomerSubscription
+    {
+        public int? Id { get; set; }
+        public int SubscriptionId { get; set; }
+        public int CustomerId { get; set; }
+
+        public DateTime SubscriptionStartDate { get; set; }
+        public DateTime? SubscriptionEndDate { get; set; }
+    }
+}
