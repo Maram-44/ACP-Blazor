c91ec85f8b20ab0ecd718c41670d3b231417bc74
Maram-44 <maramaljoohy@gmail.com>
2026-05-20 22:47:41 +0300

    Blogs

---
+++ ACP/Models/Blogs/ReadBlog.cs
@@ -0,0 +1,13 @@
+﻿namespace ACP.Models.Blogs
+{
+    public class ReadBlog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Content { get; set; } = null!;
+        public string? ImagePath { get; set; }
+        public int NumberOfVote { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public bool IsVoted { get; set; }
+    }
+}
